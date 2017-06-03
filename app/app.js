37cb0f2dd7fa41de24aca44d52f94d15c41ba7df
Martin Hora <m.hora33@gmail.com>
2017-06-03 15:36:25 +0200

    Make AdminLTE devDependency

--- app/app.js
+++ app/app.js
@@ -1,3 +1,6 @@
+import 'script-loader!../node_modules/admin-lte/plugins/jQuery/jquery-2.2.3.min'
+import 'script-loader!../node_modules/admin-lte/bootstrap/js/bootstrap.min'
+import 'script-loader!../node_modules/admin-lte/dist/js/app.min'
 import reportList from './report-list'
 import Vue from 'vue'
 import Clipboard from 'clipboard'
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,87 @@
 "use strict";
 
 const path = require('path');
-const config = require('./buildConfig');
+const fs = require('fs');
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const buildConfigFile = './buildConfig.js';
+
+const config = require(buildConfigFile);
 
 const isProduction = config.env === 'production';
 
-const webpackConfig = {
-    entry: {
-        'app': './app/app.js'        
+let webpackConfig =
+{
+    entry:
+    {
+        'js/app': './app/app.js',
+        'css/app': './assets/less/app.less',
     },
-    output: {
-        path: path.join(__dirname, 'dist/js'),
-        filename: "[name].js"
+    output:
+    {
+        path: path.join(__dirname, 'dist'),
+        filename: "[name].js",
+        publicPath: '../'
     },
-    module: {
+    module:
+    {
         loaders: [
             {
                 test: [ path.join(__dirname, 'app') ],
                 loader: 'babel-loader'
+            },
+            {
+                test: /\.(png|jpg|gif|svg|ttf|eot|woff|woff2)$/,
+                loader: 'url-loader',
+                options: {
+                    name: 'assets/[name].[ext]?[hash]',
+                    limit: 10000
+                }
+            },
+            {
+                test: /\.less$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'postcss-loader', 'less-loader']
+                })
             }
         ]
-    },   
+    },
+    plugins: [
+        new ExtractTextPlugin("[name].css"),
+    ],
+    performance: {
+        hints: false
+    },
     resolve: {
-      alias: {
-        'vue$': 'vue/dist/vue.js'
-      }
+        alias: {
+            'vue$': 'vue/dist/vue.js'
+        }
     },
 };
 
 if (!isProduction) {
-    webpackConfig.devtool = "sourcemap";
-    webpackConfig.debug = true;        
+    webpackConfig.devtool = '#source-map'
 } else {
-    const UglifyJsPlugin = require("webpack/lib/optimize/UglifyJsPlugin");
-
     webpackConfig.plugins = webpackConfig.plugins || [];
-    webpackConfig.plugins.push(new UglifyJsPlugin({ minimize: true }));
+
+    webpackConfig.plugins = webpackConfig.plugins.concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        })
+    ]);
 }
 
 module.exports = webpackConfig;
