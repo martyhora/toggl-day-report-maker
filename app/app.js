d343536b88d2f8348c877927eaa9550905852ee1
Martin Hora <m.hora33@gmail.com>
2017-10-03 14:44:17 +0200

    Format code

--- app/app.js
+++ app/app.js
@@ -1,9 +1,9 @@
-import 'script-loader!../node_modules/admin-lte/plugins/jQuery/jquery-2.2.3.min'
-import 'script-loader!../node_modules/admin-lte/bootstrap/js/bootstrap.min'
-import 'script-loader!../node_modules/admin-lte/dist/js/app.min'
-import reportList from './report-list'
-import Vue from 'vue'
-import Clipboard from 'clipboard'
+import 'script-loader!../node_modules/admin-lte/plugins/jQuery/jquery-2.2.3.min';
+import 'script-loader!../node_modules/admin-lte/bootstrap/js/bootstrap.min';
+import 'script-loader!../node_modules/admin-lte/dist/js/app.min';
+import reportList from './report-list';
+import Vue from 'vue';
+import Clipboard from 'clipboard';
 
 new Clipboard('.copy-btn');
 
--- app/report-list.js
+++ app/report-list.js
@@ -1,139 +1,152 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const date = new Date();
 
 const API_URL = 'api/v1/report';
 
 const vm = {
-    data: {
-        tasks: [],
-        date: `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`,
-        token: '',
-        workspaceId: '',
-        isLoading: false,
-        errorMessageVisible: false,
+  data: {
+    tasks: [],
+    date: `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`,
+    token: '',
+    workspaceId: '',
+    isLoading: false,
+    errorMessageVisible: false,
+  },
+  created: function() {},
+  methods: {
+    hideCopyIcon: function(task) {
+      task.isCopyIconVisible = false;
     },
-    created: function() {
-    },
-    methods: {
-        hideCopyIcon: function(task) {
-            task.isCopyIconVisible = false
-        },
-        formatTime: function(dateString) {
-            const seconds = parseInt(dateString, 10);
-            let hours   = Math.floor(seconds / 3600);
-            let minutes = Math.floor((seconds - (hours * 3600)) / 60);
+    formatTime: function(dateString) {
+      const seconds = parseInt(dateString, 10);
+      let hours = Math.floor(seconds / 3600);
+      let minutes = Math.floor((seconds - hours * 3600) / 60);
 
-            if (hours < 10) {
-                hours   = `0${hours}`;
+      if (hours < 10) {
+        hours = `0${hours}`;
+      }
+      if (minutes < 10) {
+        minutes = `0${minutes}`;
+      }
+
+      return `${hours}:${minutes}`;
+    },
+    loadFromAPI: function() {
+      this.isLoading = true;
+
+      const date = this.date.split('.');
+
+      axios
+        .get(
+          `${API_URL}/${date[2]}-${date[1]}-${date[0]}/${this
+            .workspaceId}/${this.token}`
+        )
+        .then(response => {
+          this.isLoading = false;
+
+          this.errorMessageVisible = !!response.data.error;
+
+          const tasks = response.data.data.map(task => {
+            const timeHours = task.dur / 3600000;
+
+            return {
+              project: task.project,
+              time: timeHours,
+              tags: task.tags.join(','),
+              text: task.description,
+              formattedTime: timeHours * 3600,
+            };
+          });
+
+          let projs = [];
+
+          let resProjects = [];
+
+          tasks.forEach(task => {
+            const resultProjects = tasks.filter(
+              taskF => taskF.project === task.project
+            );
+
+            if (projs.indexOf(task.project) === -1 && task.project) {
+              resProjects.push({
+                project: task.project,
+                tasks: resultProjects,
+              });
             }
-            if (minutes < 10) {
-                minutes = `0${minutes}`;
-            }
 
-            return `${hours}:${minutes}`;
-        },
-        loadFromAPI: function() {
-            this.isLoading = true;
-            
-            const date = this.date.split('.');
+            projs.push(task.project);
+          });
 
-            axios.get(`${API_URL}/${date[2]}-${date[1]}-${date[0]}/${this.workspaceId}/${this.token}`)
-                .then(response => {
-                    this.isLoading = false;
+          this.tasks = resProjects.map(project => {
+            let tasks = [];
 
-                    this.errorMessageVisible = !!response.data.error;
+            let tags = [];
 
-                    const tasks = response.data.data.map(task => {
-                        const timeHours = task.dur / 3600000;
+            let totalTimeProject = 0;
 
-                        return {
-                            project: task.project,
-                            time: timeHours,
-                            tags: task.tags.join(","),
-                            text: task.description,
-                            formattedTime: timeHours * 3600
-                        }
-                    });
+            project.tasks.forEach(task => {
+              let rmTasks = [];
 
-                    let projs = [];
+              if (tags.indexOf(task.tags) === -1) {
+                rmTasks = project.tasks.filter(
+                  taskF => taskF.tags === task.tags
+                );
 
-                    let resProjects = [];
+                let totalTime = 0;
+                let texts = [];
 
-                    tasks.forEach(task => {
-                        const resultProjects = tasks.filter(taskF => taskF.project === task.project);
+                rmTasks.forEach(rmTask => {
+                  totalTime += rmTask.time;
 
-                        if (projs.indexOf(task.project) === -1 && task.project) {
-                            resProjects.push({project: task.project, tasks: resultProjects})
-                        }
+                  if (texts.indexOf(rmTask.text) === -1) {
+                    texts.push(rmTask.text);
+                  }
+                });
 
-                        projs.push(task.project)
-                    });
+                tasks.push({
+                  project: task.project,
+                  text: texts.join('<br>'),
+                  textToCopy: texts.join('\n'),
+                  time: totalTime,
+                  formattedTime: this.formatTime(totalTime * 3600),
+                  tags: task.tags,
+                  isCopyIconVisible: true,
+                });
 
-                    this.tasks = resProjects.map(project => {
-                        let tasks = [];
+                totalTimeProject += totalTime;
+              }
 
-                        let tags = [];
-
-                        let totalTimeProject = 0;
-
-                        project.tasks.forEach(task => {
-                            let rmTasks = [];
-
-                            if (tags.indexOf(task.tags) === -1)
-                            {
-                                rmTasks = project.tasks.filter(taskF => taskF.tags === task.tags);
-
-                                let totalTime = 0;
-                                let texts = [];
-
-                                rmTasks.forEach(rmTask => {
-                                    totalTime += rmTask.time;
-
-                                    if (texts.indexOf(rmTask.text) === -1) {
-                                        texts.push(rmTask.text)
-                                    }
-                                });
-
-                                tasks.push({
-                                    project: task.project,
-                                    text: texts.join("<br>"),
-                                    textToCopy: texts.join("\n"),
-                                    time: totalTime,
-                                    formattedTime: this.formatTime(totalTime * 3600),
-                                    tags: task.tags,
-                                    isCopyIconVisible: true,
-                                });
-
-                                totalTimeProject += totalTime
-                            }
-
-                            tags.push(task.tags)
-                        });
-
-                        return { project: project.project, tasks: tasks, formattedTime: this.formatTime(totalTimeProject * 3600) }
-                    });
-                })
-                .catch(error => {
-                    this.isLoading = false;
-
-                    this.errorMessageVisible = true;
-                })
-        }
-    },
-    computed: {
-        totalTime: function() {
-            let total = 0;
-
-            this.tasks.forEach(task => {
-                task.tasks.forEach(taskT => {
-                    total += parseFloat(taskT.time)
-                })
+              tags.push(task.tags);
             });
 
-            return this.formatTime(total * 3600)
-        }
-    }
+            return {
+              project: project.project,
+              tasks: tasks,
+              formattedTime: this.formatTime(totalTimeProject * 3600),
+            };
+          });
+        })
+        .catch(error => {
+          this.isLoading = false;
+
+          this.errorMessageVisible = true;
+        });
+    },
+  },
+  computed: {
+    totalTime: function() {
+      let total = 0;
+
+      this.tasks.forEach(task => {
+        task.tasks.forEach(taskT => {
+          total += parseFloat(taskT.time);
+        });
+      });
+
+      return this.formatTime(total * 3600);
+    },
+  },
 };
 
-export default vm
+export default vm;
